b4d7eab1109967205b40e6ab0299e44389f5e747
Devin <devinpquinn4@gmail.com>
2026-08-12 22:25:18 -0400

    interim

--- Assets/Scripts/SimChecker.cs
+++ Assets/Scripts/SimChecker.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class SimChecker : MonoBehaviour
 {
@@ -7,8 +9,12 @@ public class SimChecker : MonoBehaviour
     
     public void CheckSimilarity()
     {
+        var stopwatch = Stopwatch.StartNew();
         float similarity = SimilarityHandler.GetSimilarity(sentenceA, sentenceB);
+        stopwatch.Stop();
+
         string hex = ColorUtility.ToHtmlStringRGB(Color.Lerp(Color.red, Color.green, Mathf.Clamp01(similarity)));
-        Debug.Log($"Similarity between \"{sentenceA}\" and \"{sentenceB}\": <color=#{hex}>{similarity}</color>");
+        
+        Debug.Log($"Similarity between \"{sentenceA}\" and \"{sentenceB}\": <color=#{hex}>{similarity}</color> (took {stopwatch.Elapsed.TotalMilliseconds:F2} ms)");
     }
 }
